8554b85f3c6fa21fbe771d5fc5b7139a0c70e3a1
pengchsky <pengchsky@163.com>
2024-01-31 15:05:02 +0800

    Add files via upload

---
+++ .postcssrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "plugins": {
+      // to edit target browsers: use "browserslist" field in package.json
+      "autoprefixer": {},
+      "postcss-pxtorem": {
+        rootValue: 75,
+        propList: ['*']
+      },
+    }
+  }
+  
